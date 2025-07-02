e94743f1eebbab3273cde744c512e0e84ecf726d
rahimpirbhai <73482276+rahimpirbhai@users.noreply.github.com>
2025-07-02 11:41:53 -0400

    Adding redis

---
+++ redis_client.py
@@ -0,0 +1,43 @@
+import redis
+import json
+
+import logging.config
+
+import yaml
+
+log = logging.getLogger(__name__)
+
+class RedisClient:
+    def __init__(self):
+        self.redis_client = redis.Redis()
+        self.initialized = True
+        log.debug("Initialized")
+
+    def store_call(self, func_name:str, args:[], return_value):
+        log.debug(f"{func_name}, {args}, {return_value}")
+        self.redis_client.set(name=f"{func_name}:{json.dumps(args)}", value=json.dumps(return_value), ex=3600)
+
+    def get_return_value(self, func_name:str, args:[]):
+        log.debug(f"{func_name}, {args}")
+        return self.redis_client.get(f"{func_name}:{json.dumps(args)}")
+
+    def show_all(self):
+        all_keys = self.redis_client.keys("*")
+
+        for key in all_keys:
+            value = self.redis_client.get(key)
+            print(f"{key.decode()}: {value.decode()}")
+
+
+"""
+Test
+"""
+if __name__ == "__main__":
+    with open('config/logger.yaml', 'r') as stream:
+        config = yaml.load(stream, Loader=yaml.FullLoader)
+    logging.config.dictConfig(config)
+
+    r = RedisClient()
+    r.store_call('get_l3vpn_interface_details', ['2001::225', 'l3vpn_test', 'if_test'], {'port_id': '1/1/1', 'ip_addr': '1.1.1.1/30'})
+
+    r.show_all()
---
+++ redis_play.py
@@ -0,0 +1,72 @@
+import json
+import redis
+from typing import Optional
+
+
+class RedisCache:
+    def __init__(self, host='localhost', port=6379, db=0, default_ttl=3600):
+        self.client = redis.StrictRedis(host=host, port=port, db=db, decode_responses=True)
+        self.default_ttl = default_ttl  # seconds
+
+    def get(self, key: str) -> Optional[dict]:
+        value = self.client.get(key)
+        return json.loads(value) if value else None
+
+    def get_keys(self) -> []:
+        return r.keys("*")
+
+    def show_all(self):
+        all_keys = r.keys("*")
+
+        for key in all_keys:
+            value = r.get(key)
+            print(f"{key.decode()}: {value.decode()}")
+
+    def set(self, key: str, value: dict, ttl: Optional[int] = None):
+        value_str = json.dumps(value)
+        self.client.setex(key, ttl or self.default_ttl, value_str)
+
+    def delete(self, key: str):
+        self.client.delete(key)
+
+    def exists(self, key: str) -> bool:
+        return self.client.exists(key) == 1
+
+
+if __name__ == '__main__':
+    # Test server is available/up
+    r = redis.Redis()
+    print(r.ping())  # Should print True
+    """
+    True
+    """
+
+    c = RedisCache()
+    # Add
+    #c.set("my_key", {"val": 42})
+    print(c.get("my_key"))
+    """
+    {'val': 42}
+    """
+
+    # Exists
+    print(c.exists("my_key"))
+    """
+    True
+    """
+
+    # Delete
+    #c.delete("my_key")
+
+    #print(c.exists("my_key"))
+    """
+    False
+    """
+
+    c.show_all()
+
+    print(c.get_keys())
+
+
+
+
